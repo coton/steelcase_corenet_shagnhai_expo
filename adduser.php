bb14cd76d4a603c0dacd52dc84940cb02302242d
coton_chen <coton.chen@gmail.com>
2015-10-28 18:54:20 +0800

    fix adduser.php

--- adduser.php
+++ adduser.php
@@ -10,7 +10,7 @@
 	$product= $_POST['product'];
 
 	$query = "INSERT INTO user (name, company, phone, email, area, product) 
-				VALUES('中文','$_POST[company]','$_POST[phone]','$_POST[email]','$_POST[area]','$_POST[product]')";
+				VALUES('$_POST[name]','$_POST[company]','$_POST[phone]','$_POST[email]','$_POST[area]','$_POST[product]')";
 
 	$result = mysqli_query($conn , $query) or die("Error in query: $query. ".mysql_error());  
 	mysqli_close($conn); 
